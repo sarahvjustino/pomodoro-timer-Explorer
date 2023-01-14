6bbb28026d64bcdef2875ed49984e0da13542461
Sarah <sarahvictoria230@gmail.com>
2023-01-13 21:20:15 -0300

    Modularização do código

--- js/index.js
+++ js/index.js
@@ -1,25 +1,23 @@
+import resetControls from "./controls.js";
+import { Timer } from "./timer.js";
+
 const buttonPlay = document.querySelector('.play');
 const buttonPause = document.querySelector('.pause');
 const buttonStop = document.querySelector('.stop');
 const buttonSet = document.querySelector('.set');
 const buttonSoundOn = document.querySelector('.sound-on');
 const buttonSoundOff = document.querySelector('.sound-off');
-let minutes
 let minutesDisplay = document.querySelector('#minutes');
 let secondsDisplay = document.querySelector('#seconds');
+let minutes = minutesDisplay.textContent;
+let timerTimeOut
 
-function contdown() {
-    setTimeout(() => {
-        let seconds = Number(secondsDisplay.textContent);
-
-        if (seconds <= 0) {
-            seconds = 60;
-        };
-
-        secondsDisplay.textContent = seconds - 1;
-        contdown();
-    }, 1000)
-}
+const timer = Timer({
+    minutesDisplay,
+    secondsDisplay,
+    timerTimeOut,
+    resetControls,
+})
 
 buttonPlay.addEventListener('click', () => {
     buttonPlay.classList.add('hide');
@@ -27,19 +25,18 @@ buttonPlay.addEventListener('click', () => {
     buttonSet.classList.add('hide');
     buttonStop.classList.remove('hide');
 
-    contdown()
+    timer.countdown()
 })
 
 buttonPause.addEventListener('click', () => {
     buttonPause.classList.add('hide');
     buttonPlay.classList.remove('hide');
+    clearTimeout(timerTimeOut)
 })
 
 buttonStop.addEventListener('click', () => {
-    buttonPlay.classList.remove('hide');
-    buttonPause.classList.add('hide');
-    buttonSet.classList.remove('hide');
-    buttonStop.classList.add('hide');
+    resetControls()
+    timer.resetTimer()
 })
 
 buttonSoundOn.addEventListener('click', () => {
@@ -53,6 +50,12 @@ buttonSoundOff.addEventListener('click', () => {
 })
 
 buttonSet.addEventListener('click', () => {
-    minutes = prompt('Quantos Minutos?');
-    minutesDisplay.textContent = minutes;
+    let newMinutes = prompt('Quantos Minutos?');
+    if (!newMinutes) {
+        timer.resetTimer()
+        return
+    }
+
+    minutes = newMinutes
+    updateTimerDisplay(minutes, 0)
 })
